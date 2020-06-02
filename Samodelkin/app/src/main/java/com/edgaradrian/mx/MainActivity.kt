82060d9857efc0ab7c73ad1092c0b0d4ba9cc5a0
Edgar Adrian <adick.teddy@gmail.com>
2020-06-02 18:07:27 -0500

    Refactoring to displayCharacterData (MainActivity.kt)

--- Samodelkin/app/src/main/java/com/edgaradrian/mx/MainActivity.kt
+++ Samodelkin/app/src/main/java/com/edgaradrian/mx/MainActivity.kt
@@ -3,6 +3,7 @@ package com.edgaradrian.mx
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,12 +13,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val nameTextView = findViewById<TextView>(R.id.nameTextView)
-        val raceTextView = findViewById<TextView>(R.id.raceTextView)
-        val dexterityTextView = findViewById<TextView>(R.id.dexterityTextView)
-        val wisdomTextView = findViewById<TextView>(R.id.wisdomTextView)
-        val strengthTextView = findViewById<TextView>(R.id.strengthTextView)
+        displayCharacterData()
 
+    }//onCreate
+
+    private fun displayCharacterData() {
         characterData.run {
             nameTextView.text = name
             raceTextView.text = race
@@ -25,7 +25,6 @@ class MainActivity : AppCompatActivity() {
             wisdomTextView.text = wis
             strengthTextView.text = str
         }
-
-    }//onCreate
+    }//displayCharacterData
 
 }//MainActivity
