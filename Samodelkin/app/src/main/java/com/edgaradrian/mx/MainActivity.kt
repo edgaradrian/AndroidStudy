9c8995b89eefc75990662e10598d65a00873a15a
Edgar Adrian <adick.teddy@gmail.com>
2020-06-02 17:33:53 -0500

    Looking up view elements (Main Activity)

--- Samodelkin/app/src/main/java/com/edgaradrian/mx/MainActivity.kt
+++ Samodelkin/app/src/main/java/com/edgaradrian/mx/MainActivity.kt
@@ -2,10 +2,17 @@ package com.edgaradrian.mx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nameTextView = findViewById<TextView>(R.id.nameTextView)
+        val raceTextView = findViewById<TextView>(R.id.raceTextView)
+        val dexterityTextView = findViewById<TextView>(R.id.dexterityTextView)
+        val wisdomTextView = findViewById<TextView>(R.id.wisdomTextView)
+        val strengthTextView = findViewById<TextView>(R.id.strengthTextView)
     }
-}
+}//MainActivity
