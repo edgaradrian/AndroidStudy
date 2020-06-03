a9f704b50d131855f18a7b1b3a27a083234328b4
Edgar Adrian <adick.teddy@gmail.com>
2020-06-02 20:08:25 -0500

    Defining a characterData extension property (MainActivity.kt)

--- Samodelkin/app/src/main/java/com/edgaradrian/mx/MainActivity.kt
+++ Samodelkin/app/src/main/java/com/edgaradrian/mx/MainActivity.kt
@@ -6,6 +6,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 private const val CHARACTER_DATA_KEY = "CHARACTER_DATA_KEY"
 
+private var Bundle.characterData
+    get() = getSerializable(CHARACTER_DATA_KEY) as CharacterGenerator.CharacterData
+    set(value) = putSerializable(CHARACTER_DATA_KEY, value)
+
 class MainActivity : AppCompatActivity() {
 
     private var characterData = CharacterGenerator.generate()
