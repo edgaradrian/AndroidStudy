1a581b8a034edf5a098709de8098ffd1f4a1f530
Edgar Adrian <adick.teddy@gmail.com>
2020-06-02 20:13:45 -0500

    Using the new extension property (MainActivity.kt)

--- Samodelkin/app/src/main/java/com/edgaradrian/mx/MainActivity.kt
+++ Samodelkin/app/src/main/java/com/edgaradrian/mx/MainActivity.kt
@@ -16,16 +16,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putSerializable(CHARACTER_DATA_KEY, characterData)
+        outState.characterData = characterData
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        characterData = savedInstanceState?.let {
-            it.getSerializable(CHARACTER_DATA_KEY) as CharacterGenerator.CharacterData
-        } ?: CharacterGenerator.generate()
+        characterData = savedInstanceState?.characterData ?: CharacterGenerator.generate()
 
         generateButton.setOnClickListener {
             characterData = CharacterGenerator.generate()
