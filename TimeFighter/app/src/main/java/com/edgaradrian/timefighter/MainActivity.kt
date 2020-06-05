e905244e007c314bbbe5eeec4d3038ed1d517a38
Edgar Adrián <adick.teddy@gmail.com>
2020-06-05 15:58:19 -0500

    Updating strings.xml

--- TimeFighter/app/src/main/java/com/edgaradrian/timefighter/MainActivity.kt
+++ TimeFighter/app/src/main/java/com/edgaradrian/timefighter/MainActivity.kt
@@ -11,13 +11,27 @@ class MainActivity : AppCompatActivity() {
     private lateinit var timeLeftTextView: TextView
     private lateinit var tapMeButton: Button
 
+    private var score = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        gameScoreTextview = findViewById(R.id.game_score_text_view)
+        timeLeftTextView = findViewById(R.id.time_left_text_view)
+        tapMeButton = findViewById(R.id.tap_me_button)
+
+        tapMeButton.setOnClickListener {
+            incrementScore()
+        }
+
     }//onCreate
 
     private fun incrementScore() {
+        score++
 
+        val newScore = "Your Score: $score"
+        gameScoreTextview.text = newScore
     }//incrementScore
 
     private fun resetGame() {
