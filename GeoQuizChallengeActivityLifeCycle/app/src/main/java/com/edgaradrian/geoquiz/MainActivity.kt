7b254de3ec685182f5479afc5849dfa925bcd8f3
Edgar Adrián <adick.teddy@gmail.com>
2020-06-25 17:20:06 -0500

    Challenge: Preventing Repeat Answers

---
+++ GeoQuizChallengeActivityLifeCycle/app/src/main/java/com/edgaradrian/geoquiz/MainActivity.kt
@@ -0,0 +1,109 @@
+package com.edgaradrian.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+private const val TAG = "MainActivity.kt"
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+    private var isPressed = false
+
+    private val questionBank = listOf(
+        Question(R.string.question_vietnam, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate(Bundle?) called")
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
+
+        trueButton.setOnClickListener { view: View ->
+            checkAnswer(true)
+        }//trueButton onClickListener
+
+        falseButton.setOnClickListener { view: View ->
+            checkAnswer(false)
+        }//falseButton onClickListener
+
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        updateQuestion()
+
+    }//onCreate
+
+    private fun updateQuestion() {
+        isPressed = false
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }//updateQuestion
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        if (!isPressed) {
+            val correctAnswer = questionBank[currentIndex].answer
+
+            val messageResId = if (userAnswer == correctAnswer) {
+                R.string.correct_toast
+            } else {
+                R.string.incorrect_toast
+            }
+
+            Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).apply {
+                setGravity(Gravity.TOP, 0, 300)
+                show()
+            }
+        }
+        isPressed = true
+    }//checkAnswer
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }//onStart
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }//onResume
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }//onPause
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }//onStop
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }//onDestroy
+
+}//MainActivity
