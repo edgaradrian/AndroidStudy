5639f9f7aaa0c3a1e2bf064f61b0888d35575ab3
Edgar Adrián <adick.teddy@gmail.com>
2020-06-26 14:39:19 -0500

    Creating a ViewModal class (QuizViewModel.kt)

---
+++ GeoQuiz/app/src/main/java/com/edgaradrian/geoquiz/QuizViewModel.kt
@@ -0,0 +1,19 @@
+package com.edgaradrian.geoquiz
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+private const val TAG = "QuizViewModel"
+
+class QuizViewModel: ViewModel() {
+
+    init {
+        Log.d(TAG, "ViewModel instance created")
+    }//init
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d(TAG, "ViewModel instance about to be destroyed")
+    }//onCleared
+    
+}//QuizViewModel
