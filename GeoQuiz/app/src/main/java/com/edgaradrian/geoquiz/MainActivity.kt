1bec41a4078d339e568ad1a95078b99031353456
Edgar Adrián <adick.teddy@gmail.com>
2020-06-27 14:23:31 -0500

    Checking bundle in onCreate (MainActivity.kt)

--- GeoQuiz/app/src/main/java/com/edgaradrian/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/edgaradrian/geoquiz/MainActivity.kt
@@ -3,6 +3,7 @@ package com.edgaradrian.geoquiz
 import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.Gravity
 import android.view.View
@@ -89,6 +90,12 @@ class MainActivity : AppCompatActivity() {
         super.onPause()
         Log.d(TAG, "onPause() called")
     }//onPause
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.i(TAG, "onSaveInstanceState")
+        outState.putInt(KEY_INDEX, quizViewModel.currentIndex)
+    }//onSaveInstanceState
 
     override fun onStop() {
         super.onStop()
