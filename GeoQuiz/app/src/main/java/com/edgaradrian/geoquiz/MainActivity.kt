28667165748e4bf169684672beffd64e973e6584
Edgar Adrián <adick.teddy@gmail.com>
2020-07-02 13:51:29 -0500

    Wiring up the cheat button (MainActivity.kt)

--- GeoQuiz/app/src/main/java/com/edgaradrian/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/edgaradrian/geoquiz/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
     private lateinit var questionTextView: TextView
+    private lateinit var cheatButton: Button
 
     private val quizViewModel: QuizViewModel by lazy {
         ViewModelProviders.of(this).get(QuizViewModel::class.java)
@@ -39,6 +40,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        cheatButton = findViewById(R.id.cheat_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
@@ -52,6 +54,10 @@ class MainActivity : AppCompatActivity() {
         nextButton.setOnClickListener {
             quizViewModel.moveToNext()
             updateQuestion()
+        }
+
+        cheatButton.setOnClickListener {
+            
         }
 
         updateQuestion()
