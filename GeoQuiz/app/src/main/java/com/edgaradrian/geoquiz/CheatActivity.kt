42492bfa7be1d418ac3a7dcc3bb95baafbae2b86
Edgar Adrián <adick.teddy@gmail.com>
2020-07-02 14:36:00 -0500

    A newIntent(...) function for CheatActivity (CheatActivity.kt)

--- GeoQuiz/app/src/main/java/com/edgaradrian/geoquiz/CheatActivity.kt
+++ GeoQuiz/app/src/main/java/com/edgaradrian/geoquiz/CheatActivity.kt
@@ -1,13 +1,25 @@
 package com.edgaradrian.geoquiz
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 private const val EXTRA_ANSWER_IS_TRUE = "com.edgaradrian.android.geoquiz.answer_is_true"
 
 class CheatActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cheat)
     }//onCreate
+
+    companion object {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            }
+        }//newIntent
+    }//companion object
+
 }//CheatActivity
