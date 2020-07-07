3d2fec286e66757d3b3ddacbf4afd6fb1fd77834
Edgar Adrián <adick.teddy@gmail.com>
2020-07-07 15:38:55 -0500

    Overriding onCreateView(...) (CrimeFragment.kt)

--- LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/CrimeFragment.kt
+++ LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/CrimeFragment.kt
@@ -1,6 +1,9 @@
 package dev.edgaradrian.locriminalintent
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class CrimeFragment: Fragment() {
@@ -10,7 +13,14 @@ class CrimeFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
-
     }//onCreate
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+    }
 
 }//CrimeFragment
