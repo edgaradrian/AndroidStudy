d1c4f4d6dfb67f50109fc65b17ab5de105984645
Edgar Adrián <adick.teddy@gmail.com>
2020-07-07 16:42:07 -0500

    Listening for CheckBox changes (CrimeFragment.kt)

--- LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/CrimeFragment.kt
+++ LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/CrimeFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 
@@ -15,6 +16,7 @@ class CrimeFragment: Fragment() {
     private lateinit var crime: Crime
     private lateinit var titleField: EditText
     private lateinit var dateButton: Button
+    private lateinit var solvedCheckBox: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +31,7 @@ class CrimeFragment: Fragment() {
         val view = inflater.inflate(R.layout.fragment_crime, container, false)
         titleField = view.findViewById(R.id.crime_title) as EditText
         dateButton = view.findViewById(R.id.crime_date) as Button
+        solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
 
         dateButton.apply {
             text = crime.date.toString()
@@ -58,6 +61,12 @@ class CrimeFragment: Fragment() {
         }//titleWatcher
 
         titleField.addTextChangedListener(titleWatcher)
+
+        solvedCheckBox.apply {
+            setOnCheckedChangeListener { _, isChecked ->
+                crime.isSolved = isChecked
+            }
+        }
 
     }//onStart
 
