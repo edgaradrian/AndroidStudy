b470e7741e8a14863a1ee0365c9b6ecf07d12557
Edgar Adrián <adick.teddy@gmail.com>
2020-07-07 15:24:57 -0500

    Overriding Fragment.onCreate(Bundle?) (CrimeFragment.kt)

--- LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/CrimeFragment.kt
+++ LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/CrimeFragment.kt
@@ -1,7 +1,16 @@
 package dev.edgaradrian.locriminalintent
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 
 class CrimeFragment: Fragment() {
-    
+
+    private lateinit var crime: Crime
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+
+    }//onCreate
+
 }//CrimeFragment
