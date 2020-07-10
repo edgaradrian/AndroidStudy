02c7b6dc8449d0b460c4ff107b150db20cee299d
Edgar Adrián <adick.teddy@gmail.com>
2020-07-09 19:40:25 -0500

    Setting up the view for CrimeListFragment (CrimeListFragment.kt)

--- LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/CrimeListFragment.kt
+++ LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/CrimeListFragment.kt
@@ -2,13 +2,19 @@ package dev.edgaradrian.locriminalintent
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 private const val TAG = "CrimeListFragment"
 
 class CrimeListFragment: Fragment() {
+
+    private lateinit var crimeRecyclerView: RecyclerView
 
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
@@ -18,6 +24,18 @@ class CrimeListFragment: Fragment() {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
     }//onCreate
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
+
+        crimeRecyclerView = view.findViewById(R.id.crime_recycler_view) as RecyclerView
+        crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+        return view
+    }
 
     companion object {
         fun newInstance(): CrimeListFragment {
