fc898cebe0cbf2007d398a4dda3b893dffc02577
Edgar Adrián <adick.teddy@gmail.com>
2020-07-14 17:58:14 -0500

    Setting up repository properties (CrimeRepository.kt)

--- LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/CrimeRepository.kt
+++ LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/CrimeRepository.kt
@@ -1,9 +1,21 @@
 package dev.edgaradrian.locriminalintent
 
 import android.content.Context
+import androidx.room.Room
+import dev.edgaradrian.locriminalintent.database.CrimeDatabase
 import java.lang.IllegalStateException
 
+private const val DATABASE_NAME = "crime-database"
+
 class CrimeRepository private constructor(context: Context) {
+
+    private val database: CrimeDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        CrimeDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    private val crimeDao = database.crimeDao()
 
     companion object {
         private var INSTANCE: CrimeRepository? = null
