9df1b188af7a0b5396cbcb327c5aa54164cb893e
Edgar Adrián <adick.teddy@gmail.com>
2020-07-14 16:24:19 -0500

    Adding database query functions (database/CrimeDao.kt)

--- LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/database/CrimeDao.kt
+++ LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/database/CrimeDao.kt
@@ -1,7 +1,17 @@
 package dev.edgaradrian.locriminalintent.database
 
 import androidx.room.Dao
+import androidx.room.Query
+import dev.edgaradrian.locriminalintent.Crime
+import java.util.*
 
 @Dao
 interface CrimeDao {
-}
+
+    @Query("SELECT * FROM crime")
+    fun getCrimes(): List<Crime>
+
+    @Query("SELECT * FROM crime WHERE id=(:id)")
+    fun getCrime(id: UUID): Crime?
+
+}//CrimeDao
