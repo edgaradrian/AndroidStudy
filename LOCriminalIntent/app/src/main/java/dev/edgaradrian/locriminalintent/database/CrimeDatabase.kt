67d1fd6865f72d3cc608c35bb8e8c99dc74f6217
Edgar Adrián <adick.teddy@gmail.com>
2020-07-14 15:48:35 -0500

    Adding TypeConverter functions (database/CrimeTypeConverter.kt)

--- LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/database/CrimeDatabase.kt
+++ LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/database/CrimeDatabase.kt
@@ -2,9 +2,11 @@ package dev.edgaradrian.locriminalintent.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import dev.edgaradrian.locriminalintent.Crime
 
 @Database(entities = [ Crime::class ], version = 1)
+@TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase: RoomDatabase() {
-
+    
 }
