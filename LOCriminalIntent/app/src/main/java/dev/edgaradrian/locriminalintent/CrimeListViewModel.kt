f9c112609979614102ca41c36ebcc10f655a126b
Edgar Adrián <adick.teddy@gmail.com>
2020-07-16 17:21:15 -0500

    Accessing the repository in the ViewModel (CrimeListViewModel.kt)

--- LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/CrimeListViewModel.kt
+++ LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/CrimeListViewModel.kt
@@ -5,6 +5,6 @@ import androidx.lifecycle.ViewModel
 class CrimeListViewModel: ViewModel() {
 
    private val crimeRepository = CrimeRepository.get()
-   val crimes = crimeRepository.getCrimes()
+   val crimeListLiveData = crimeRepository.getCrimes()
 
 }//CrimeListViewModel
