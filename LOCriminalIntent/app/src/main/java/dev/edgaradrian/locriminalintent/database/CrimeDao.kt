b09f18f18266cbad0956ae2d78c07ebf2b89fe4e
Edgar Adrián <adick.teddy@gmail.com>
2020-07-16 17:12:22 -0500

    Returning LiveData in the DAO (database/CrimeDao.kt)

--- LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/database/CrimeDao.kt
+++ LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/database/CrimeDao.kt
@@ -1,5 +1,6 @@
 package dev.edgaradrian.locriminalintent.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
 import dev.edgaradrian.locriminalintent.Crime
@@ -9,9 +10,9 @@ import java.util.*
 interface CrimeDao {
 
     @Query("SELECT * FROM crime")
-    fun getCrimes(): List<Crime>
+    fun getCrimes(): LiveData<List<Crime>>
 
     @Query("SELECT * FROM crime WHERE id=(:id)")
-    fun getCrime(id: UUID): Crime?
+    fun getCrime(id: UUID): LiveData<Crime?>
 
 }//CrimeDao
