a44f552f07a369a691c3f16feda1e51632e7febd
Edgar Adrián <adick.teddy@gmail.com>
2020-08-04 19:23:35 -0500

    Adding update and insert database functions (database/CrimeDao.kt)

--- LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/database/CrimeDao.kt
+++ LOCriminalIntent/app/src/main/java/dev/edgaradrian/locriminalintent/database/CrimeDao.kt
@@ -2,7 +2,9 @@ package dev.edgaradrian.locriminalintent.database
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import dev.edgaradrian.locriminalintent.Crime
 import java.util.*
 
@@ -14,5 +16,11 @@ interface CrimeDao {
 
     @Query("SELECT * FROM crime WHERE id=(:id)")
     fun getCrime(id: UUID): LiveData<Crime?>
+
+    @Update
+    fun updateCrime(crime: Crime)
+
+    @Insert
+    fun addCrime(crime: Crime)
 
 }//CrimeDao
