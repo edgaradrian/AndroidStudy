bc035e84dc55ca7cc4ee029542ee9dd5c37174ff
Edgar Adrián <adick.teddy@gmail.com>
2020-08-06 19:41:38 -0500

    Adding padding modifier to Column

--- Compose/ComposeNewStory/app/src/main/java/dev/edgaradrian/composenewstory/MainActivity.kt
+++ Compose/ComposeNewStory/app/src/main/java/dev/edgaradrian/composenewstory/MainActivity.kt
@@ -3,12 +3,15 @@ package dev.edgaradrian.composenewstory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.ui.core.Modifier
 import androidx.ui.core.setContent
 import androidx.ui.foundation.Text
 import androidx.ui.layout.Column
+import androidx.ui.layout.padding
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.Surface
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
 import dev.edgaradrian.composenewstory.ui.ComposeNewStoryTheme
 
 class MainActivity : AppCompatActivity() {
@@ -22,7 +25,9 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun NewsStory() {
-    Column {
+    Column (
+        modifier = Modifier.padding(16.dp)
+    ){
         Text("A day in Pantitlán")
         Text("México, Distrito Federal")
         Text("Agosto 2020")
