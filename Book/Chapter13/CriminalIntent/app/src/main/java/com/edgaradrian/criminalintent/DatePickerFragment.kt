4139721123ffd1139d43b4ff6de7bb94938619be
Edgar Adrian <adick.teddy@gmail.com>
2021-01-21 23:00:08 -0600

    Adding a newInstance(Date) function (DatePickerFragment.kt)

--- Book/Chapter13/CriminalIntent/app/src/main/java/com/edgaradrian/criminalintent/DatePickerFragment.kt
+++ Book/Chapter13/CriminalIntent/app/src/main/java/com/edgaradrian/criminalintent/DatePickerFragment.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import java.util.*
 
+private const val ARG_DATE = "date"
+
 class DatePickerFragment : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -21,5 +23,18 @@ class DatePickerFragment : DialogFragment() {
             initialDay
         )
     }//onCreateDialog
+
+    companion object {
+        fun newInstance(date: Date): DatePickerFragment {
+            val args = Bundle().apply {
+                putSerializable(ARG_DATE, date)
+            }
+
+            return DatePickerFragment().apply {
+                arguments = args
+            }
+
+        }//newInstance
+    }//companion object
 
 }//DatePickerFragment
