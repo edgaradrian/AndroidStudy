04c9d51aedcd9b27c7de7145a7de57f696b98538
Edgar Adrian <adick.teddy@gmail.com>
2021-01-21 22:10:20 -0600

    Chapter 13 start

---
+++ Book/Chapter13/CriminalIntent/app/src/main/java/com/edgaradrian/criminalintent/CriminalIntentApplication.kt
@@ -0,0 +1,12 @@
+package com.edgaradrian.criminalintent
+
+import android.app.Application
+
+class CriminalIntentApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        CrimeRepository.initialize(this)
+    }
+
+}
