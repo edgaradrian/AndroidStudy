0c9a295f5931533be039bb24c3d15abac89b4fcf
Edgar Adrian <adick.teddy@gmail.com>
2021-01-22 19:32:52 -0600

    Sending back the date (DatePickerFragment.kt)

--- Book/Chapter13/CriminalIntent/app/src/main/java/com/edgaradrian/criminalintent/DatePickerFragment.kt
+++ Book/Chapter13/CriminalIntent/app/src/main/java/com/edgaradrian/criminalintent/DatePickerFragment.kt
@@ -3,8 +3,10 @@ package com.edgaradrian.criminalintent
 import android.app.DatePickerDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.widget.DatePicker
 import androidx.fragment.app.DialogFragment
 import java.util.*
+import javax.security.auth.callback.Callback
 
 private const val ARG_DATE = "date"
 
@@ -15,6 +17,19 @@ class DatePickerFragment : DialogFragment() {
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        val dateListener = DatePickerDialog.OnDateSetListener {
+            _: DatePicker, year: Int, month: Int, day: Int ->
+
+            val resultDate: Date = GregorianCalendar(year, month, day).time
+
+            targetFragment?.let { fragment ->
+                (fragment as Callbacks).onDateSelected(resultDate)
+            }
+
+        }
+
+
         val date = arguments?.getSerializable(ARG_DATE) as Date
         val calendar = Calendar.getInstance()
         calendar.time = date
@@ -23,7 +38,7 @@ class DatePickerFragment : DialogFragment() {
         val initialDay = calendar.get(Calendar.DAY_OF_MONTH)
         return DatePickerDialog(
             requireContext(),
-            null,
+            dateListener,
             initialYear,
             initialMonth,
             initialDay
