631bebb2debdf598b0d621ef3270738f47b6fdda
Edgar Adrian <adick.teddy@gmail.com>
2021-01-26 16:57:18 -0600

    Guarding against no contacts app (CrimeFragment.kt)

--- Book/Chapter15/CriminalIntent/app/src/main/java/com/edgaradrian/criminalintent/CrimeFragment.kt
+++ Book/Chapter15/CriminalIntent/app/src/main/java/com/edgaradrian/criminalintent/CrimeFragment.kt
@@ -2,6 +2,8 @@ package com.edgaradrian.criminalintent
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
 import android.net.Uri
 import android.os.Bundle
 import android.provider.ContactsContract
@@ -129,7 +131,16 @@ class CrimeFragment: Fragment(), DatePickerFragment.Callbacks {
                 startActivityForResult(pickContactIntent, REQUEST_CONTACT)
             }
 
-        }
+            
+            val packageManager: PackageManager = requireActivity().packageManager
+            val resolvedActivity: ResolveInfo? = packageManager.resolveActivity(pickContactIntent, PackageManager.MATCH_DEFAULT_ONLY)
+
+            if (resolvedActivity == null) {
+                isEnabled = false
+            }
+
+
+        }//suspect
 
     }//onStart
 
