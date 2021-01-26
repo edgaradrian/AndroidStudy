0122f506c7fb37521bc8712e427fce1aad0b3197
Edgar Adrian <adick.teddy@gmail.com>
2021-01-26 13:13:12 -0600

    Pulling the contact's name out (CrimeFragment.kt)

--- Book/Chapter15/CriminalIntent/app/src/main/java/com/edgaradrian/criminalintent/CrimeFragment.kt
+++ Book/Chapter15/CriminalIntent/app/src/main/java/com/edgaradrian/criminalintent/CrimeFragment.kt
@@ -1,7 +1,8 @@
 package com.edgaradrian.criminalintent
 
-import android.app.ProgressDialog.show
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.text.Editable
@@ -153,6 +154,33 @@ class CrimeFragment: Fragment(), DatePickerFragment.Callbacks {
             suspectButton.text = crime.suspect
         }
     }//updateUI
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when {
+            resultCode != Activity.RESULT_OK -> return
+
+            resultCode == REQUEST_CONTACT && data != null -> {
+                val contactUri: Uri? = data.data
+
+                val queryFields = arrayOf(ContactsContract.Contacts.DISPLAY_NAME)
+
+                val cursor = requireActivity().contentResolver.query(contactUri!!, queryFields, null, null, null)
+
+                cursor?.use {
+
+                    if (it.count == 0) {
+                        return
+                    }
+
+                    it.moveToFirst()
+                    val suspect = it.getString(0)
+                    crime.suspect = suspect
+                    crimeDetailViewModel.saveCrime(crime)
+                    suspectButton.text = suspect
+                }
+            }
+        }
+    }//onActivityResult
 
     private fun getCrimeReport(): String {
         val solvedString = if (crime.isSolved) {
