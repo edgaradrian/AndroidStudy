57eabe9ee65f0ced0f5977d26ad6983dc647c67e
Edgar Adrian <adick.teddy@gmail.com>
2021-01-27 17:10:25 -0600

    Writing a conservative scale function (PictureUtils.kt)

--- Book/Chapter15/CriminalIntent/app/src/main/java/com/edgaradrian/criminalintent/PictureUtils.kt
+++ Book/Chapter15/CriminalIntent/app/src/main/java/com/edgaradrian/criminalintent/PictureUtils.kt
@@ -1,7 +1,18 @@
 package com.edgaradrian.criminalintent
 
+import android.app.Activity
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Point
+
+fun getScaledBitmap(path: String, activity: Activity): Bitmap {
+
+    val size = Point()
+    activity.windowManager.defaultDisplay.getSize(size)
+
+    return getScaledBitmap(path, size.x, size.y)
+
+}//getScaledBitmap
 
 fun getScaledBitmap(path:String, destWidth: Int, destHeight: Int): Bitmap {
 
