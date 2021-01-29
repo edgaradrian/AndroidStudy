4c0fca70b4e07020611d2695f9f11f55e7ca7b28
Edgar Adrian <adick.teddy@gmail.com>
2021-01-29 15:22:37 -0600

    Configuring RecyclerView (MainActivity.kt)

--- Book/Chapter19/BeatBox/app/src/main/java/com/edgaradrian/beatbox/MainActivity.kt
+++ Book/Chapter19/BeatBox/app/src/main/java/com/edgaradrian/beatbox/MainActivity.kt
@@ -3,6 +3,7 @@ package com.edgaradrian.beatbox
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
 import com.edgaradrian.beatbox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.recyclerView.apply {
+            layoutManager = GridLayoutManager(context, 3)
+        }
     }//onCreate
 
-    
+
 
 }//MainActivity
