b705f4e5ebefd8376fd39f614ac9d3ed2d7beb35
Edgar Adrian <adick.teddy@gmail.com>
2021-01-31 23:33:45 -0600

    Creating a SoundViewModel test subject (SoundViewModelTest.kt)

---
+++ Book/Chapter19/BeatBox/app/src/test/java/com/edgaradrian/beatbox/SoundViewModelTest.kt
@@ -0,0 +1,18 @@
+package com.edgaradrian.beatbox
+
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class SoundViewModelTest {
+
+    private lateinit var sound: Sound
+    private lateinit var subject: SoundViewModel
+
+    @Before
+    fun setUp() {
+        sound = Sound("assetPath")
+        subject = SoundViewModel()
+        subject.sound= sound
+    }//setUp
+}
