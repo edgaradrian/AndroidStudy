42f71242269018625208187df66c8cf763717ec4
Edgar Adrian <adick.teddy@gmail.com>
2021-02-03 11:08:38 -0600

    Chapter 21 init

---
+++ Book/Chapter21/BeatBox/app/src/main/java/com/edgaradrian/beatbox/SoundViewModel.kt
@@ -0,0 +1,25 @@
+package com.edgaradrian.beatbox
+
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+
+class SoundViewModel(private val beatBox: BeatBox) : BaseObservable() {
+
+    var sound: Sound? = null
+        set(sound) {
+            field = sound
+            notifyChange()
+        }
+
+   @get:Bindable
+    val title: String?
+        get() = sound?.name
+
+
+    fun onButtonClicked() {
+        sound?.let {
+            beatBox.play(it)
+        }
+    }//onButtonClicked
+
+}//class SoundViewModel
