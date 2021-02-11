8da3a9dfb8679d510593615827c36c9956fcebcc
Edgar Adrian <adick.teddy@gmail.com>
2021-02-11 00:52:46 -0600

    Implementing RecyclerView.Adapter (NerdLauncherActivity.kt)

--- Book/Chapter23/NerdLauncher/app/src/main/java/com/edgaradrian/nerdlauncher/NerdLauncherActivity.kt
+++ Book/Chapter23/NerdLauncher/app/src/main/java/com/edgaradrian/nerdlauncher/NerdLauncherActivity.kt
@@ -5,7 +5,9 @@ import android.content.pm.ResolveInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -58,6 +60,25 @@ class NerdLauncherActivity : AppCompatActivity() {
         }//bindActivity
 
     }//ActivityHolder
+
+    private class ActivityAdapter(val activities: List<ResolveInfo>): RecyclerView.Adapter<ActivityHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActivityHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+            return ActivityHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ActivityHolder, position: Int) {
+            val resolveInfo = activities[position]
+            holder.bindActivity(resolveInfo)
+        }
+
+        override fun getItemCount(): Int {
+            return activities.size
+        }
+
+    }//ActivityAdapter
 
 
 }//AppCompatActivity
