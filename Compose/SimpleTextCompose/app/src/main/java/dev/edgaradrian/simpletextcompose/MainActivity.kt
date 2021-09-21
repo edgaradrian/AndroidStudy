1f05649e9f50d2a2cbb2e216c882f1b30d33b0b6
Edgar Adrián <adick.teddy@gmail.com>
2021-09-21 11:05:51 -0500

    Setting style in Text

--- Compose/SimpleTextCompose/app/src/main/java/dev/edgaradrian/simpletextcompose/MainActivity.kt
+++ Compose/SimpleTextCompose/app/src/main/java/dev/edgaradrian/simpletextcompose/MainActivity.kt
@@ -3,27 +3,51 @@ package dev.edgaradrian.simpletextcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.dp
 import dev.edgaradrian.simpletextcompose.ui.theme.SimpleTextComposeTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalUnitApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent(content = {
-            //GreetingText(name = "enfermeraaaaa!!!")
-            GreetingButton()
+            GreetingText(name = "enfermeraaaaa!!!")
+            //GreetingButton()
         })
     }
 }
 
+@ExperimentalUnitApi
 @Composable
 fun GreetingText(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!",
+        modifier = Modifier.width(240.dp)
+            .height(240.dp)
+            .clickable {  }
+            .padding(24.dp),
+        style = TextStyle(color = Color.Magenta,
+        fontWeight = FontWeight.Bold,
+            fontSize = TextUnit(18.0F, TextUnitType.Sp)
+        )
+    )
 }
 
+@ExperimentalUnitApi
 @Composable
 fun GreetingButton() {
     Button(onClick = {
@@ -33,11 +57,12 @@ fun GreetingButton() {
     }
 }
 
+@ExperimentalUnitApi
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SimpleTextComposeTheme {
-        //GreetingText("Android")
-        GreetingButton()
+        GreetingText("enfermera")
+        //GreetingButton()
     }
 }
