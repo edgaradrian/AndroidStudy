b1145509be414e1bdaa1a4ff85e86466d87689ed
Edgar Adrián <ingeniero@skiff.com>
2023-10-11 15:56:42 -0600

    Challenge: Switching Your Toast for a Snackbar

--- FifthEdition/CopyGeoQuiz/app/src/main/java/com/edgaradrian/geoquiz/MainActivity.kt
+++ FifthEdition/CopyGeoQuiz/app/src/main/java/com/edgaradrian/geoquiz/MainActivity.kt
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,11 +20,13 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
 
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+            Snackbar.make(findViewById(R.id.linear_layout_main) ,
+                R.string.correct_toast, Snackbar.LENGTH_SHORT).show()
         }
 
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+            Snackbar.make(findViewById(R.id.linear_layout_main),
+                R.string.incorrect_toast, Snackbar.LENGTH_SHORT).show()
         }
 
     }//onCreate
