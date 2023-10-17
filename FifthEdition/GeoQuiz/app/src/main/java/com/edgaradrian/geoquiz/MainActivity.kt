50a9ebd0160f90939e40839752ba96a2ffab5499
Edgar Adrián <ingeniero@skiff.com>
2023-10-17 15:01:25 -0600

    Initializing ActivityMainBinding (MainActivity.kt)

--- FifthEdition/GeoQuiz/app/src/main/java/com/edgaradrian/geoquiz/MainActivity.kt
+++ FifthEdition/GeoQuiz/app/src/main/java/com/edgaradrian/geoquiz/MainActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.edgaradrian.geoquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
@@ -24,7 +27,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
